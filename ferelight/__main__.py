50af6a9acd1359a9333942d01faba79f840423af
Florian Spiess <florian.spiess@unibas.ch>
2025-06-04 14:33:29 +0100

    CORS configuration support (#12)
    * Implement configuration of CORS requests
    
    * Update README to describe new CORS config parameters
    
    * Bump version

--- ferelight/__main__.py
+++ ferelight/__main__.py
@@ -3,8 +3,28 @@ import argparse
 import json
 
 import connexion
+from flask import current_app
 
 from ferelight import encoder
+
+
+def add_cors_headers(response):
+    """Add CORS headers to the response if enabled in the configuration."""
+    cors_config = current_app.config.get('CORS', {})
+
+    if cors_config.get('ENABLED', False):
+        response.headers['Access-Control-Allow-Origin'] = cors_config.get('ALLOW_ORIGIN', '*')
+        response.headers['Access-Control-Allow-Methods'] = cors_config.get('ALLOW_METHODS', 'GET, POST, OPTIONS')
+        response.headers['Access-Control-Allow-Headers'] = cors_config.get('ALLOW_HEADERS', 'Content-Type, Authorization')
+
+    return response
+
+
+def handle_options_request():
+    """Handle OPTIONS requests for CORS preflight."""
+    response = current_app.make_default_options_response()
+    add_cors_headers(response)
+    return response
 
 
 def main():
@@ -21,6 +41,11 @@ def main():
                 arguments={'title': 'FERElight'},
                 pythonic_params=True)
     app.app.config.from_file(config_path, load=json.load)
+
+    # Register CORS-related handlers
+    app.app.after_request(add_cors_headers)
+    app.app.route('/<path:path>', methods=['OPTIONS'])(handle_options_request)
+    app.app.route('/', methods=['OPTIONS'])(handle_options_request)
 
     app.run(port=8080)
 
